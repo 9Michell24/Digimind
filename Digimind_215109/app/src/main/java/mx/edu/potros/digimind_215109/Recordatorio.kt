e8530b6feb348c891a56537c1d4dc43e497a6b4a
michelle <michellebojorquezsterling@gmail.com>
2025-06-25 11:50:00 -0700

    parte 2 finalizada y correcta

--- Digimind_215109/app/src/main/java/mx/edu/potros/digimind_215109/Recordatorio.kt
+++
@@ -1,5 +0,0 @@
-package mx.edu.potros.digimind_215109
-import java.io.Serializable
-
-data class Recordatorio (var dias: String, var tiempo: String, var nombre: String)  : Serializable
-
---
+++ MyDigimind_215109/app/src/main/java/mx/edu/potros/mydigimind_215109/ui/Recordatorio.kt
@@ -0,0 +1,7 @@
+package mx.edu.potros.mydigimind_215109.ui
+
+import java.io.Serializable
+
+data class Recordatorio (var dias: String,
+                         var tiempo: String,
+                         var nombre: String) : Serializable
